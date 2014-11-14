ab83a3dc218a93d370167e0ea2aa7fb6aa1b78b4
Leon Grasmeijer <Leon@Leons-MacBook-Air.local>
2014-11-14 15:03:55 +0100

    Added script

---
+++ script.js
@@ -0,0 +1,38 @@
+readDoc(document.body);
+
+function readDoc(node) 
+{
+	// I stole this function from here:
+	// http://is.gd/mwZp7E
+	
+	var child, next;
+
+	switch ( node.nodeType )  
+	{
+		case 1:  // Element
+		case 9:  // Document
+		case 11: // Document fragment
+			child = node.firstChild;
+			while ( child ) 
+			{
+				next = child.nextSibling;
+				walk(child);
+				child = next;
+			}
+			break;
+
+		case 3: // Text node
+			changeWord(node);
+			break;
+	}
+}
+
+function changeWord(textNode) 
+{
+	var v = textNode.nodeValue;
+
+	v = v.replace(/\bBug\b/g, "Feature");
+	v = v.replace(/\bbug\b/g, "feature");
+	
+	textNode.nodeValue = v;
+}
